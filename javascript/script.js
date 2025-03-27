6dbc14d45182c8357e46e10d633ae986f16b6825
elin17 <elin.guinchoma@gmail.com>
2025-03-27 14:38:29 +1100

    Added js element.

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+function updateTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]:SSS A[</small]"
+  );
+
+  // Melbourne
+  let melbourneElement = document.querySelector("#melbourne");
+  let melbourneDateElement = melbourneElement.querySelector(".date");
+  let melbourneTimeElement = melbourneElement.querySelector(".time");
+  let melbourneTime = moment().tz("Australia/Melbourne");
+
+  melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+  melbourneTimeElement.innerHTML = melbourneTime.format(
+    "h:mm:ss [<small>]:SSS A[</small]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1);
